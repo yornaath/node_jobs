05d4473e9d823c1d98ce5d695871c861ee8355a6
andtan <jornandretangen@gmail.com>
2011-12-01 16:01:07 +0100

    adapter.save and adapter.destry takes job as arg

--- src/persistence_adapters/mongodb_adapter/index.js
+++ src/persistence_adapters/mongodb_adapter/index.js
@@ -17,31 +17,53 @@ var Adapter = (function() {
       url:                    {type: String},
       state:                  {type: String},
       scheduled_at_timestamp: {type: Number},
-      scheduled_for_every:    {type: Number}
+      scheduled_for_every:    {type: Number},
+      persisted:              {type: Boolean}
     }))
   }
 
   Adapter.prototype.getAll = function(cb) {
-    var docs = []
-    for(jid in mockdb) {
-      docs.push(mockdb[jid])
+    this.model.find(function(err, specs) {
+      if(err) return cb.call(this, err)
+      cb.call(this, false, specs)
+    })
+  }
+
+  Adapter.prototype.save = function(job, cb) {
+    var model = this.model,
+        jobspec = job.generateDbRepresentation(),
+        _job = new model;
+    for(key in jobspec) {
+      _job[key] = jobspec[key]
     }
-    cb(false, docs)
+    _job.save(function(err) {
+      if(err) return cb.call(this, err)
+      job._id = _job._id
+      job.persisted = true
+      return cb.call(this, false, job)
+    })
   }
 
-  Adapter.prototype.save = function(cb) {
-    var _job = this.generateDbRepresentation()
-    _job._id = mockid++
-    mockdb[_job._id] = _job
-    cb(false)
-  }
-
-  Adapter.prototype.destroy = function(cb) {
-    cb(false)
+  Adapter.prototype.destroy = function(job, cb) {
+    var model = this.model;
+    if(job._id && job.persisted) {
+      model.find({_id: job._id}, function(err) {
+        if(err) return cb.call(this, err)
+        return cb.call(this, false)
+      })
+    }
+    return cb.call(this, 'Cannot call destroy on unpersisted job') 
   }  
 
   return Adapter
-})
+})();
 
 
 module.exports = Adapter
+
+
+
+
+
+
+
